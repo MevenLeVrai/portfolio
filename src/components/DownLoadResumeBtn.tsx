6ceef86892bd5e53c046fb9788156b7b3ca242d0
Meven <meven.desbois@icloud.com>
2026-04-11 15:02:54 +0200

    Add i18n to download resume's button

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,18 +1,17 @@
-import { cn } from '@/lib/utils'
-import { Download } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-import { buttonVariants } from './ui/button'
+"use client"
 import HackerBtn from './animation/HackerBtn'
+import { useTranslations } from 'next-intl'
 
 function DownLoadResumeBtn() {
+    const t = useTranslations("home");
+
     return (
         <div className="h-fit w-full mt-2 py-2 px-4">
             <a
                 href="/resume-meven-desbois.pdf"
                 download="Meven_Desbois_Resume.pdf"
             >
-                <HackerBtn label="Download Resume" />
+                <HackerBtn label={t("downloadResume")} />
             </a>
         </div>
     )
