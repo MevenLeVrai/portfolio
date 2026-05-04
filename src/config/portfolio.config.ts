69eb12c4ac36419281402161bdb068157f812274
MevenLeVrai <meven.desbois@icloud.com>
2026-05-04 21:45:51 +0200

    :wrench: update project list

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -105,13 +105,13 @@ export const portfolioConfig = {
 
     // Liens des projets (pas de traduction)
     projectLinks: [
+        { title: "Pokerix – Turn-based Multiplayer Battle Game", link: "https://github.com/BaptouK/PokeRixe", status: "in progress" },
+        { title: "CTF & Security Labs", link: "https://ctf.mevendesbois.com ", status: "new" },
         { title: "COM Cyber CTF – Operation Bellatrix", link: "https://www.defense.gouv.fr/air/actualites/orion-jeunesse-2026-loperation-bellatrix-met-au-defi-jeunes", status: "new" },
         { title: "CCNA1 Certification Passage", link: "https://www.credly.com/badges/7547886a-e643-4f05-9409-945fba2949a4/public_url", status: "new" },
-        { title: "Azure PHP App – CI/CD, Containers & MySQL", link: "", status: "new" },
+        { title: "Azure PHP App – CI/CD, Containers & MySQL", link: "", status: undefined },
         { title: "Network Infrastructure – TacTic Brother BUILD", link: "", status: undefined },
         { title: "Network Supervision & Operations – TacTic Brother RUN", link: "", status: undefined },
-        { title: "Pokerix – Turn-based Multiplayer Battle Game", link: "https://github.com/BaptouK/PokeRixe", status: "in progress" },
-        { title: "CTF & Security Labs", link: "https://ctf.mevendesbois.com ", status: "in progress" },
         { title: "ESP32 Wi‑Fi Sniffer – Advanced Packet Analysis", link: "https://github.com/MevenLeVrai/Esp32-Wifi-Sniffer", status: undefined },
         { title: "Self‑Hosted Git Forge – Forgejo on Debian", link: "", status: undefined },
     ],
